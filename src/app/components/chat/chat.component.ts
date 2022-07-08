892f0fabc85bf24af305f3c4b7c83a48758b599f
ibellon <isidro.bellon@gmail.com>
2022-07-08 19:23:31 +0200

    Cargar y agregar mensajes

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -11,11 +11,7 @@ export class ChatComponent implements OnInit {
   mensaje: string = "";
 
   constructor(public chatService:ChatService) { 
-    this.chatService.cargarMensajes().subscribe(
-      (mensajes:any[]) => {
-        console.log(mensajes);
-      }
-    );
+    this.chatService.cargarMensajes().subscribe();
   }
 
   ngOnInit(): void {
@@ -23,6 +19,14 @@ export class ChatComponent implements OnInit {
 
   enviarMensaje() {
     console.log(this.mensaje);
+
+    if(this.mensaje.length === 0) {
+      return;
+    }
+
+    this.chatService.agregarMensaje(this.mensaje)
+      .then(() => this.mensaje = "")
+      .catch(error => console.log("Error al guardar", error));
   }
 
 }
--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -1,20 +1,38 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import { Mensaje } from '../interfaces/mensaje.interface';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatService {
 
-  public chats:any[] = [];
+  public chats:Mensaje[] = [];
 
-  private itemsCollection: AngularFirestoreCollection<any>;
+  private itemsCollection: AngularFirestoreCollection<Mensaje>;
 
   constructor(private afs: AngularFirestore) { 
-    this.itemsCollection = this.afs.collection<any>('chats');
+    this.itemsCollection = this.afs.collection<Mensaje>('chats');
   }
 
   cargarMensajes() {  
-    return this.itemsCollection.valueChanges();
+    return this.itemsCollection.valueChanges()
+    .pipe(map( (mensajes: Mensaje[]) => {
+      console.log(mensajes);
+      this.chats = mensajes;                       
+    }));                       
+  }
+
+  agregarMensaje(texto: string) {
+
+    //TODO Falta el uid del usuario
+    let mensaje: Mensaje = {
+      nombre: 'Isidro',
+      mensaje: texto,
+      fecha: new Date().getTime()
+    }
+
+    return this.itemsCollection.add(mensaje);
   }
 }
